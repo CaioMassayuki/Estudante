eb7b26b71c29267497c2a064e0bdad7db8f1ae79
CaioMassayuki <caio.yamashiro@easynvest.com.br>
2018-12-20 10:56:01 -0200

    Classes do ES6 e Maneiras de Renderizar

--- ReactNinja/Moludo1/ReactWebPack/src/app.js
+++ ReactNinja/Moludo1/ReactWebPack/src/app.js
@@ -1,16 +1,29 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Title from './title'
 
-const App = React.createClass({
-    render: function () {
+class App extends Component {
+    //this.props
+    render() {
         return (
             <div>
-                <Title lastname={15+10}/>
+                <Title lastname={15 + 10} />
             </div>
         )
-    } 
-})
+    }
+}
+
+App.defaultProps
+
+// const App = React.createClass({
+//     render: function () {
+//         return (
+//             <div>
+//                 <Title lastname={15+10}/>
+//             </div>
+//         )
+//     } 
+// })
 
 export default App
