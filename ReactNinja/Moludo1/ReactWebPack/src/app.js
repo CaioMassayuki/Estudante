7f3279c66c16dac0eb425d854825f1ad091dfed1
CaioMassayuki <caio.yamashiro@easynvest.com.br>
2018-12-20 10:24:09 -0200

    Default Props

--- ReactNinja/Moludo1/ReactWebPack/src/app.js
+++ ReactNinja/Moludo1/ReactWebPack/src/app.js
@@ -7,7 +7,7 @@ const App = React.createClass({
     render: function () {
         return (
             <div className='container'>
-                <Title name='Caio Massayuki' />
+                <Title />
                 <label htmlFor='input'>Input</label>
                 <input type='text' id='input'></input>
             </div>
