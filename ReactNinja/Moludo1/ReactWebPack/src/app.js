ffd63154a61575fad8e9ca9087e7c2de389544a3
CaioMassayuki <japa.zito@hotmail.com>
2019-01-04 11:59:48 -0200

    From TextArea

--- ReactNinja/Moludo1/ReactWebPack/src/app.js
+++ ReactNinja/Moludo1/ReactWebPack/src/app.js
@@ -6,31 +6,16 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            value: '2'
+
         }
     }
     render() {
         return (
             <form>
-                {/* Controlled Select Component */}
-                <select value={this.state.value} onChange={(e) => {
-                    this.setState({
-                        value: e.target.value
-                    })
-                }}>
-                    <option value='1'>Opção 1</option>
-                    <option value='2'>Opção 2</option>
-                    <option value='3'>Opção 3</option>
-                </select>
-
-                <br />
-
-                {/* Uncontrolled Select Component */}
-                <select>
-                    <option value='1'>Opção 1</option>
-                    <option value='2' selected>Opção 2</option>
-                    <option value='3'>Opção 3</option>
-                </select>
+                {/* Controlled TextArea Component */}
+                <textarea value='aaaaaaa'></textarea>
+                {/* Uncontrolled TextArea Component */}
+                <textarea defaultValue='textArea value'/>
             </form>
         )
     }
