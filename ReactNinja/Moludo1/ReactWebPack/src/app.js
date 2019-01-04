b8cd5f50a9f44f7c3f99525c1f9faaee8f692570
CaioMassayuki <japa.zito@hotmail.com>
2019-01-04 10:36:12 -0200

    PropTypes

--- ReactNinja/Moludo1/ReactWebPack/src/app.js
+++ ReactNinja/Moludo1/ReactWebPack/src/app.js
@@ -1,35 +1,15 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Timer from './timer'
-
+import Button from './button'
 class App extends Component {
-    constructor () {
-        console.log('constructor')
-        super()
-        this.state = {
-            showTimer: true,
-            color: 'green',
-            time: 0
-        }
-    }
-
-    componentWillMount () {
-        console.log('componentWillMount')
-    }
-
-    componentDidMount () {
-        console.log('componentDidMount')
-    }
-
     render() {
         console.log('render')
         return (
             <div>
-                <Timer time={this.state.time}/>
-                <button onClick={() => {
-                    this.setState({ time: this.state.time + 10 })
-                }}>Change Props</button>
+                <Button handleClick={() => console.log('clicou')}>
+                    Clique em Mim!
+                </Button>
             </div>
         )
     }
