75ffd2a524f9d4233d4dd005cb121064f70e65eb
CaioMassayuki <japa.zito@hotmail.com>
2019-01-04 11:44:00 -0200

    Checkbox e Radio

--- ReactNinja/Moludo1/ReactWebPack/src/app.js
+++ ReactNinja/Moludo1/ReactWebPack/src/app.js
@@ -6,18 +6,60 @@ class App extends Component {
     constructor () {
         super()
         this.state = {
-            value: 'Valor Inicial'
+            value: 'Controlled Component',
+            checked: false,
+            radioCheck: false
         }
     }
     render() {
         return (
             <div>
                 <form>
+                    {/* Controlled TextBox Component */}
                     <input type='text' value={this.state.value} onChange={(e) => {
                         this.setState({
                             value: e.target.value
                         })
                     }}/>
+                    <label>
+                        {/* Controlled CheckBox Component */}
+                        <input type='checkbox' checked={this.state.checked} onChange={(e) => {
+                            this.setState({
+                                checked: e.target.checked
+                            })
+                        }} />
+                        Controlled Component
+                    </label>
+                    <label>
+                        {/* Controlled Radio Component */}
+                        <input type='radio' name='r' value={1} checked={this.state.radioCheck} onChange={(e) => {
+                            this.setState({
+                                radioCheck: e.target.radioCheck
+                            })
+                        }} />
+                        Controlled Component
+                    </label>
+                    <label>
+                        {/* Controlled Radio Component */}
+                        <input type='radio' name='r' value={2} checked={this.state.radioCheck} onChange={(e) => {
+                            this.setState({
+                                radioCheck: e.target.radioCheck
+                            })
+                        }} />
+                        Controlled Component
+                    </label>
+                    <br />
+
+                    {/* Uncontrolled TextBox Component */}
+                    <input type='text' defaultValue='Uncontrolled Component' />
+                    <label>
+                        {/* Uncontrolled CheckBox Component */}
+                        <input type='checkbox' defaultChecked={true} />
+                        Uncontrolled Component
+                    </label>
+                    {/* Uncontrolled Radio Component */}
+                    <input type='radio' name='d' value={1} defaultChecked /> Uncontrolled Component
+                    <input type='radio' name='d' value={2}  />Uncontrolled Component
                 </form>
             </div>
         )
