068f7fc1c69aac264c12668a2ae05a8e06a9cff4
CaioMassayuki <japa.zito@hotmail.com>
2019-01-07 11:11:47 -0200

    GitHubApp

--- ReactNinja/Moludo1/GitHubApp/src/app.js
+++ ReactNinja/Moludo1/GitHubApp/src/app.js
@@ -1,11 +1,41 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = React.createClass({
-    render: () => {
-        return <h1>Aplicação 1</h1>
-    } 
-})
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            value: '2'
+        }
+    }
+    render() {
+        return (
+            <form>
+                {/* Controlled Select Component */}
+                <select value={this.state.value} onChange={(e) => {
+                    this.setState({
+                        value: e.target.value
+                    })
+                }}>
+                    <option value='1'>Opção 1</option>
+                    <option value='2'>Opção 2</option>
+                    <option value='3'>Opção 3</option>
+                </select>
+
+                <br />
+
+                {/* Uncontrolled Select Component */}
+                <select>
+                    <option value='1'>Opção 1</option>
+                    <option value='2' selected>Opção 2</option>
+                    <option value='3'>Opção 3</option>
+                </select>
+            </form>
+        )
+    }
+}
+
+App.defaultProps
 
 export default App
