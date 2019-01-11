7fa6cfbe7543f6ab1ac86ef2fea3dd5cc7bcf7b1
CaioMassayuki <japa.zito@hotmail.com>
2019-01-11 15:30:37 -0200

    Markdown Editor HTML/MD

--- ReactNinja/Modulo2/Markdown-editor/src/app.js
+++ ReactNinja/Modulo2/Markdown-editor/src/app.js
@@ -1,14 +1,41 @@
 'use strict'
 
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
+import marked from 'marked'
+import hljs from 'highlight.js'
+import MarkdownEditor from './components/markdown-editor'
 
+import 'normalize.css'
+import 'highlight.js/styles/dracula.css'
 import './css/style.css'
 
-const App = () => (
-  <div className='editor'>
-    <textarea />
-    <div className='view' />
-  </div>
-)
+marked.setOptions({
+  highlight: (code) => {
+    return hljs.highlightAuto(code).value
+  }
+})
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      value: ''
+    }
+    this.handleChange = (e) => {
+      this.setState({
+        value: e.target.value
+      })
+    }
+    this.getMarkup = () => {
+      return { __html: marked(this.state.value) }
+    }
+  }
+
+  render () {
+    return (
+      <MarkdownEditor value={this.state.value} handleChange={this.handleChange} getMarkup={this.getMarkup} />
+    )
+  }
+}
 
 export default App
