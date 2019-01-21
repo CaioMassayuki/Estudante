51b58565f6f9086a0d65a9be7cac70f9e41448ed
CaioMassayuki <japa.zito@hotmail.com>
2019-01-21 18:40:43 -0200

    Primeiros Testes - Aula 1

--- ReactNinja/Modulo3/hello-redux/main.js
+++ ReactNinja/Modulo3/hello-redux/main.js
@@ -1,13 +1,11 @@
 'use strict'
 
-const counter = (state, action) => {
-    if (action.type === 'INCREMENT'){
-        return state + 1
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT': return state + 1
+        case 'DECREMENT': return state - 1
     }
 
-    if (action.type === 'DECREMENT'){
-        return state - 1
-    }
     return state
 }
 
@@ -27,3 +25,10 @@ console.assert(
     counter(5, { type: 'DECREMENT' }) === 4
 )
 
+console.assert(
+    counter(3, { type: 'SOMETHING' }) === 3
+)
+
+console.assert(
+    counter(undefined, {}) === 0
+)
