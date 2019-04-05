49544c2fefb8dabafba6913b2755af609239c827
CaioMassayuki <japa.zito@hotmail.com>
2019-04-04 21:33:16 -0300

    MarkdownEditor

---
+++ ReactMarkdownEditor/markdown-editor/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import MarkdownEditor from "./components/markdown-editor"
+import "./App.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MarkdownEditor />
+      </div>
+    )
+  }
+}
+
+export default App
--- ReactMarkdownEditor/markdowneditor/src/components/textbox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Textbox = ({ style, handleTextValueChange }) => (
-  <div style={style.Container}>
-    <textarea style={style.Textarea} onChange={handleTextValueChange} />
-  </div>
-)
-
-export default Textbox
